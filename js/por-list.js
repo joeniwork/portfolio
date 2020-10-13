cfce296f7e62bb89f37e45cbf11d5c5154088cfa
joeniwork <joeniwork@gmail.com>
2020-10-13 21:41:12 +0800

    images-edit
    路徑錯誤

--- js/por-list.js
+++ js/por-list.js
@@ -1,111 +1,111 @@
 var websiteLists=[
   {
-    img: "../images/porfolio/w21.jpg",
+    img: "images/porfolio/w21.jpg",
     name: "C C 專刊-圓滿9月",
     weblink: true,
     link: "https://reurl.cc/VX0Aj5",
     modal: "web21"
   },
   {
-    img: "../images/porfolio/w19.jpg",
+    img: "images/porfolio/w19.jpg",
     name: "高雄音樂製作中心",
     weblink: true,
     link: "http://soft-world-music.tw/index.html",
     modal: "web01"
   },
   {
-    img: "../images/porfolio/w22.jpg",
+    img: "images/porfolio/w22.jpg",
     name: "C C 專刊-萬聖10月",
     weblink: true,
     link: "https://reurl.cc/e8GR6W",
     modal: "web22"
   },
   {
-    img: "../images/porfolio/w20.jpg",
+    img: "images/porfolio/w20.jpg",
     name: "翻轉音樂活動網站",
     weblink: false,
     link: "Demo提案",
     modal: "web02"
   },
   {
-    img: "../images/porfolio/w01.jpg",
+    img: "images/porfolio/w01.jpg",
     name: "正益工業",
     weblink: false,
     link: "網站已神隱",
     modal: "web03"
   },
   {
-    img: "../images/porfolio/w24.jpg",
+    img: "images/porfolio/w24.jpg",
     name: "C C 專刊-聖誕12月",
     weblink: true,
     link: "https://reurl.cc/Q35rjZ",
     modal: "web24"
   },
   {
-    img: "../images/porfolio/w02.jpg",
+    img: "images/porfolio/w02.jpg",
     name: "利到創新科技",
     weblink: false,
     link: "網站已神隱",
     modal: "web04"
   },
   {
-    img: "../images/porfolio/w23.jpg",
+    img: "images/porfolio/w23.jpg",
     name: "C C 專刊-感恩11月",
     weblink: true,
     link: "https://reurl.cc/WLA6kD",
     modal: "web23"
   },
   {
-    img: "../images/porfolio/w03.jpg",
+    img: "images/porfolio/w03.jpg",
     name: "昌澤機械",
     weblink: true,
     link: "http://new.changtjer.com.tw/tw/main/",
     modal: "web05"
   },
   {
-    img: "../images/porfolio/w04.jpg",
+    img: "images/porfolio/w04.jpg",
     name: "軒登國際",
     weblink: false,
     link: "網站已神隱",
     modal: "web06"
   },
   {
-    img: "../images/porfolio/w05.jpg",
+    img: "images/porfolio/w05.jpg",
     name: "統全興業",
     weblink: false,
     link: "網站已神隱",
     modal: "web07"
   },
   {
-    img: "../images/porfolio/w06.jpg",
+    img: "images/porfolio/w06.jpg",
     name: "皇旗內衣",
     weblink: false,
     link: "網站已神隱",
     modal: "web08"
   },
   {
-    img: "../images/porfolio/w07.jpg",
+    img: "images/porfolio/w07.jpg",
     name: "昱泓工業",
     weblink: true,
     link: "http://yuhub.com.tw/en/main/",
     modal: "web09"
   },
   {
-    img: "../images/porfolio/w11.jpg",
+    img: "images/porfolio/w11.jpg",
     name: "陳啟川紀念館",
     weblink: false,
     link: "Demo提案",
     modal: "web16"
   },
   {
-    img: "../images/porfolio/w13.jpg",
+    img: "images/porfolio/w13.jpg",
     name: "亞庫洛司",
     weblink: true,
     link: "http://www.akroshair.com.tw/",
     modal: "web18"
   },
   {
-    img: "../images/porfolio/w15.jpg",
+    img: "images/porfolio/w15.jpg",
     name: "櫻花語言工作坊",
     weblink: false,
     link: "網站已神隱",
@@ -115,112 +115,112 @@ var websiteLists=[
 
 var designLists=[
   {
-    img: "../images/porfolio/de01.jpg",
+    img: "images/porfolio/de01.jpg",
     name: "8月EIP系統 員工活動",
     link: "登入畫面設計",
     modal: "de01",
     modal_size: "modal-lg"
   },
   {
-    img: "../images/porfolio/de02.jpg",
+    img: "images/porfolio/de02.jpg",
     name: "12月EIP系統 聖誕節慶",
     link: "登入畫面設計",
     modal: "de02",
     modal_size: "modal-lg"
   },
   {
-    img: "../images/porfolio/de03.jpg",
+    img: "images/porfolio/de03.jpg",
     name: "6月EIP系統 端午節慶",
     link: "登入畫面設計",
     modal: "de03",
     modal_size: "modal-lg"
   },
   {
-    img: "../images/porfolio/de04.jpg",
+    img: "images/porfolio/de04.jpg",
     name: "會議規範",
     link: "公司文宣設計",
     modal: "de04",
     modal_size: "modal-lg"
   },
   {
-    img: "../images/porfolio/de05.jpg",
+    img: "images/porfolio/de05.jpg",
     name: "無限創意凝聚",
     link: "公司文宣設計",
     modal: "de05",
     modal_size: "modal-sm"
   },
   {
-    img: "../images/porfolio/de06.jpg",
+    img: "images/porfolio/de06.jpg",
     name: "ColorfulParty尾牙拍貼機",
     link: "公司文宣設計",
     modal: "de06",
     modal_size: "modal-lg"
   },
   {
-    img: "../images/porfolio/de07.jpg",
+    img: "images/porfolio/de07.jpg",
     name: "雙11節慶登入頁 提案1",
     link: "公司文宣設計",
     modal: "de07",
     modal_size: "modal-lg"
   },
   {
-    img: "../images/porfolio/de08.jpg",
+    img: "images/porfolio/de08.jpg",
     name: "雙11節慶登入頁",
     link: "公司文宣設計",
     modal: "de08",
     modal_size: "modal-lg"
   },
   {
-    img: "../images/porfolio/de09.jpg",
+    img: "images/porfolio/de09.jpg",
     name: "智冠音樂中心錄音室-宣傳DM",
     link: "公司文宣設計",
     modal: "de09",
     modal_size: "modal-sm"
   },
   {
-    img: "../images/porfolio/de10.jpg",
+    img: "images/porfolio/de10.jpg",
     name: "智冠音樂多媒體中心-宣傳DM",
     link: "公司文宣設計",
     modal: "de10",
     modal_size: "modal-sm"
   },
   {
-    img: "../images/porfolio/de11.jpg",
+    img: "images/porfolio/de11.jpg",
     name: "電玩主題曲-YouTube封面",
     link: "公司文宣設計",
     modal: "de11",
     modal_size: "modal-lg"
   },
   {
-    img: "../images/porfolio/de12.jpg",
+    img: "images/porfolio/de12.jpg",
     name: "手遊節慶網路宣傳",
     link: "公司文宣設計",
     modal: "de12",
     modal_size: "modal-lg"
   },
   {
-    img: "../images/porfolio/de13.jpg",
+    img: "images/porfolio/de13.jpg",
     name: "音樂多媒體部門icon字體設計",
     link: "LOGO設計",
     modal: "de13",
     modal_size: "modal-sm"
   },
   {
-    img: "../images/porfolio/de14.jpg",
+    img: "images/porfolio/de14.jpg",
     name: "智冠影音中心LOGO-提案1",
     link: "LOGO設計",
     modal: "de14",
     modal_size: "modal-sm"
   },
   {
-    img: "../images/porfolio/de15.jpg",
+    img: "images/porfolio/de15.jpg",
     name: "智冠影音中心LOGO-提案2",
     link: "LOGO設計",
     modal: "de15",
     modal_size: "modal-sm"
   },
   {
-    img: "../images/porfolio/de16.jpg",
+    img: "images/porfolio/de16.jpg",
     name: "Nobollel-立旗設計",
     link: "公司文宣設計",
     modal: "de16",
@@ -230,60 +230,60 @@ var designLists=[
 
 var skill_txList=[
   {
-    icon: "../images/sublime.svg",
+    icon: "images/sublime.svg",
     name: "Subline"
   },
   {
-    icon: "../images/brackets.svg",
+    icon: "images/brackets.svg",
     name: "Brackets"
   },
   {
-    icon: "../images/vs_code.svg",
+    icon: "images/vs_code.svg",
     name: "VS Code"
   }
 ];
 
 var skill_dsList=[
   {
-    icon: "../images/adobe-ps.svg",
+    icon: "images/adobe-ps.svg",
     name: "Photoshop"
   },
   {
-    icon: "../images/adobe-ai.svg",
+    icon: "images/adobe-ai.svg",
     name: "Illustrator"
   },
   {
-    icon: "../images/adobe-xd.svg",
+    icon: "images/adobe-xd.svg",
     name: "Adobe XD"
   }
 ];
 
 var skill_websList=[
   {
-    icon: "../images/html5.svg",
+    icon: "images/html5.svg",
     name: "Html & css"
   },
   {
-    icon: "../images/sass.svg",
+    icon: "images/sass.svg",
     name: "Sass"
   },
   {
-    icon: "../images/bootstrap.svg",
+    icon: "images/bootstrap.svg",
     name: "Bootstrap"
   }
 ];
 
 var skill_gitsList=[
   {
-    icon: "../images/tortoisesvn.png",
+    icon: "images/tortoisesvn.png",
     name: "tortoise svn"
   },
   {
-    icon: "../images/sourcetree.svg",
+    icon: "images/sourcetree.svg",
     name: "Sourcetree"
   },
   {
-    icon: "../images/gitlab.svg",
+    icon: "images/gitlab.svg",
     name: "Git Lab"
   }
 ];
